c7385c3a061945dd5be3153437374ca6fae78d3d
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-04-08 21:44:46 +0300

    update user helper functions that CREATES user

--- modals/users.js
+++ modals/users.js
@@ -1,18 +1,21 @@
 const { v4 } = require('uuid');
 const md5 = require('md5');
 
-function createUser(userName, password) {
+function createUser(userName, password, firstName, lastName, sessionId) {
     const newUser = {};
     newUser.id = v4();
     newUser.picture = './img/default_userpic.webp';
     newUser.userName = userName;
-    newUser.password = password;
-    newUser.firstName = '';
-    newUser.lastName = '';
+    newUser.password = md5(password);
+    newUser.firstName = firstName;
+    newUser.lastName = lastName;
+    newUser.session = sessionId;
     newUser.role = 'user';
 
     return newUser;
 }
+
+
 
 module.exports = {
     createUser
