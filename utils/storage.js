1ca5afde075acaa1f5e0c25f8b15d1ec6570928f
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-04-08 15:19:32 +0300

    Create helper function to write the json file

--- utils/storage.js
+++ utils/storage.js
@@ -6,10 +6,18 @@ function getUsers() {
     return jsData;
 }
 
+function setUsers(users) {
+    fs.writeFileSync('./data/users.json', JSON.stringify(users), 'utf-8');
+}
+
 function getIdeas() {
     const jsonData = fs.readFileSync('./data/ideas.json', 'utf-8');
     const jsData = JSON.parse(jsonData);
     return jsData;
+}
+
+function setIdeas(users) {
+    fs.writeFileSync('./data/ideas.json', JSON.stringify(users), 'utf-8');
 }
 
 function getDonations() {
@@ -18,8 +26,16 @@ function getDonations() {
     return jsData;
 }
 
+function setDonations(users) {
+    fs.writeFileSync('./data/donations.json', JSON.stringify(users), 'utf-8');
+}
+
+
 module.exports = {
     getUsers,
+    setUsers,
     getIdeas,
+    setIdeas,
     getDonations,
+    setDonations,
 }
