030c9e997ea58588c76a49f91cdaaccf5b2f294e
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-04-09 12:41:43 +0300

    Update user helper function that deletes idea

--- modals/ideas.js
+++ modals/ideas.js
@@ -3,9 +3,9 @@ const { getIdeas, setIdeas } = require('./utils/storage.js');
 function deleteIdea(ideaId) {
     let ideas = getIdeas();
 
-    let ideaToDelete = ideas.filter(idea => ideaId !== idea.id);
+    let updatedIdeas = ideas.filter(idea => ideaId !== idea.id);
 
-    setIdeas(ideaToDelete);
+    setIdeas(updatedIdeas);
 }
 
 module.exports = {
