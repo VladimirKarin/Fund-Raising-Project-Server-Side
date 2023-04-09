e5cd83ed1653130f0fd8d1a74486c88c626e398c
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-04-09 13:04:01 +0300

    update user helper function that updates ideas

--- modals/ideas.js
+++ modals/ideas.js
@@ -2,9 +2,7 @@ const { getIdeas, setIdeas } = require('./ideas.js');
 
 function updateIdea(ideaId, idea) {
     let ideas = getIdeas();
-
-    const idea = ideas.find(idea => ideaId === idea.id);
-    const updateIdea = { ...idea };
+    const idea = idea;
 
     const updateIdeas = ideas.reduse((updateIdeas, idea) => {
         if (idea.id === ideaId) {
