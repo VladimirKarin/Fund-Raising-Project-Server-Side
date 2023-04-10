5997d2a379d01c2bb6d24ec788c2677c3fc00281
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-04-10 13:25:47 +0300

    Fr 36 successfull create donation function test

--- terminalApp.js
+++ terminalApp.js
@@ -1,5 +1,6 @@
 /* Testing App */
 // const { createUser } = require('./models/users.js');
+const { createDonation } = require('./models/donations');
 const { createIdea, deleteIdea } = require('./models/ideas');
 
 //FR-31 testin user creation function
@@ -17,4 +18,13 @@ const { createIdea, deleteIdea } = require('./models/ideas');
 
 //FR-34 testin idea delete function
 
-deleteIdea('6f871b10-9462-4ead-b3a3-c63505e0f5c1');
+// deleteIdea('6f871b10-9462-4ead-b3a3-c63505e0f5c1');
+
+//FR-36 testing create donation function
+
+createDonation(
+    'Master',
+    '5000',
+    '45ff012c-be0d-487e-9b9d-7f53471821b0',
+    'c1ca74ae-c42d-4b51-b814-2edcd8af35ec'
+);
