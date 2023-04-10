6e3d58614f99b5380922acdd4210e2e45a92761c
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-04-10 13:04:44 +0300

    FR 33 successfull idea creation test

--- terminalApp.js
+++ terminalApp.js
@@ -1,8 +1,16 @@
-
 /* Testing App */
-const { createUser } = require('./models/users.js');
+// const { createUser } = require('./models/users.js');
+const { createIdea } = require('./models/ideas');
 
 //FR-31 testin user creation function
 
-console.log(createUser('masterYoda', '202cb962ac59075b964b07152d234b70', 'Master', 'Yoda')); 
+// console.log(createUser('masterYoda', '202cb962ac59075b964b07152d234b70', 'Master', 'Yoda'));
 
+//FR-- testin idea creation function
+
+createIdea(
+    'New Brains2',
+    'Think about having a spare pair of new brains',
+    '200002',
+    'e6328604-cd5b-46ff-920d-ea6c91e5eb88'
+);
