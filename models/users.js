77701b58ea20fe2fe0e4f577b7eeff2e00557071
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-04-10 11:54:22 +0300

    FR-32 Successfull create user function test in

--- models/users.js
+++ models/users.js
@@ -1,9 +1,11 @@
 const { v4 } = require('uuid');
 const md5 = require('md5');
 
-const { getUsers, setUsers } = require('./utils/storage.js');
+const { getUsers, setUsers } = require('../utils/storage');
 
 function createUser(userName, password, firstName, lastName) {
+    let users = getUsers();
+
     const newUser = {};
     newUser.id = v4();
     newUser.picture = './img/default_userpic.webp';
@@ -14,23 +16,23 @@ function createUser(userName, password, firstName, lastName) {
     newUser.session = null;
     newUser.role = 'user';
 
-    return newUser;
+    const newUsers = [...users, newUser];
+    return setUsers(newUsers);
 }
 
 function deleteUser(userId) {
     let users = getUsers();
-    const userToDelete = users.find(user => userId === user.id);
+    const userToDelete = users.find((user) => userId === user.id);
 
     if (userToDelete.role === 'admin') {
         throw new Error(`You cant't delete "Admin". `);
     }
 
-    let updatedUsers = users.filter(user => userId !== user.id);
+    let updatedUsers = users.filter((user) => userId !== user.id);
     setUsers(updatedUsers);
-
 }
 
 module.exports = {
     createUser,
-    deleteUser
-}
+    deleteUser,
+};
---
+++ terminalApp.js
@@ -0,0 +1,6 @@
+/* Testing App */
+const { createUser } = require('./models/users');
+
+//FR-31 testin user creation function
+
+createUser('DVader', '202cb962ac59075b964b07152d234b70', 'Darth', 'Vader');
