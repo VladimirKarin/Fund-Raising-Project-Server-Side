42414397e46c0f5c54c01aa02ac7e0696cd9e839
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-04-25 19:21:15 +0300

    FR-45 Create a fun. that creates idea. Tested.

--- businessRules/ideas.js
+++ businessRules/ideas.js
@@ -1,4 +1,4 @@
-const { getAllIdeas } = require('../models/ideas');
+const { getAllIdeas, createIdea } = require('../models/ideas');
 
 function getIdeas() {
     return getAllIdeas();
@@ -13,7 +13,22 @@ function sortedByDonationSumIdeas() {
     return sortedIdeasList;
 }
 
+function createIdeas(header, description, askedSum, userId) {
+    if (header === null) {
+        throw new Error('Error. Please, check your header.');
+    } else if (description === null) {
+        throw new Error('Error. Please, check your description.');
+    } else if (askedSum === null) {
+        throw new Error('Error. Please, check your asked sum.');
+    } else if (userId === null) {
+        throw new Error('Error. Please, check if you are logged in.');
+    } else if (header && description && askedSum && userId) {
+        createIdea(header, description, askedSum, userId);
+    }
+}
+
 module.exports = {
     getIdeas,
     sortedByDonationSumIdeas,
+    createIdeas,
 };
--- testing/testUtilFunction.js
+++ testing/testUtilFunction.js
@@ -7,71 +7,106 @@ const { createIdea, deleteIdea } = require('../models/ideas');
 const {
     getIdeas,
     sortedByDonationSumIdeas,
+    createIdeas,
 } = require('../businessRules/ideas');
 const { donationData } = require('../businessRules/donations');
 const { registerUser } = require('../businessRules/users');
+/*
+FR-31 testin user creation function
 
-//FR-31 testin user creation function
+createUser('DVader', '202cb962ac59075b964b07152d234b70', 'Darth', 'Vader');
+console.log(createUser('masterYoda', '202cb962ac59075b964b07152d234b70', 'Master', 'Yoda'));
 
-// createUser('DVader', '202cb962ac59075b964b07152d234b70', 'Darth', 'Vader');
-// console.log(createUser('masterYoda', '202cb962ac59075b964b07152d234b70', 'Master', 'Yoda'));
+FR-32 test delete user function
 
-//FR-32 test delete user function
+deleteUser('48ba4d1a-cf29-46ea-9252-292c4c486a54');
 
-// deleteUser('48ba4d1a-cf29-46ea-9252-292c4c486a54');
+FR 33-- testing idea creation function
 
-//FR 33-- testing idea creation function
+createIdea(
+    'New Brains2',
+    'Think about having a spare pair of new brains',
+    '200002',
+    'e6328604-cd5b-46ff-920d-ea6c91e5eb88'
+);
 
-// createIdea(
-//     'New Brains2',
-//     'Think about having a spare pair of new brains',
-//     '200002',
-//     'e6328604-cd5b-46ff-920d-ea6c91e5eb88'
+FR-34 testin idea delete function
+
+deleteIdea('6f871b10-9462-4ead-b3a3-c63505e0f5c1');
+
+FR-36 testing create donation function
+
+createDonation(
+    'Master',
+    '5000',
+    '45ff012c-be0d-487e-9b9d-7f53471821b0',
+    'c1ca74ae-c42d-4b51-b814-2edcd8af35ec'
+);
+
+FR-26 testing idea update function
+updateIdea('333e02ba-8e79-472f-8441-a4da39884253', 'askedSum', 50);
+createIdea(
+    'header2',
+    'description2',
+    20,
+    'e6328604-cd5b-46ff-920d-ea6c91e5eb88'
+);
+getIdeas();
+console.log(sortedByDonationSumIdeas());
+
+DonationData Tests
+
+donationData(
+    'Darth',
+    10,
+    '6fba75be-1f13-449e-b8ee-0d9287d70208',
+    '4e94a388-268d-46c9-b888-b2279c469ecd'
 // );
+donationData(
+    null,
+    10,
+    '6fba75be-1f13-449e-b8ee-0d9287d70208',
+    '4e94a388-268d-46c9-b888-b2279c469ecd'
+);
+donationData('Vladimir', 10, null, '4e94a388-268d-46c9-b888-b2279c469ecd');
+donationData(null, 10, null, '4e94a388-268d-46c9-b888-b2279c469ecd');
 
-//FR-34 testin idea delete function
+RegisterUser Test
+registerUser('MasterYoda', '123', 'Master', 'Yoda');
+registerUser('AnonymousDroid', '123', null, 'Droid');
+registerUser('MrSpaceNalls', '123', 'Mr', null);
+registerUser('Chewbacca', '123', null, null);
 
-// deleteIdea('6f871b10-9462-4ead-b3a3-c63505e0f5c1');
 
-//FR-36 testing create donation function
-
-// createDonation(
-//     'Master',
-//     '5000',
-//     '45ff012c-be0d-487e-9b9d-7f53471821b0',
-//     'c1ca74ae-c42d-4b51-b814-2edcd8af35ec'
-// );
-
-//FR-26 testing idea update function
-// updateIdea('333e02ba-8e79-472f-8441-a4da39884253', 'askedSum', 50);
-// createIdea(
-//     'header2',
-//     'description2',
-//     20,
-//     'e6328604-cd5b-46ff-920d-ea6c91e5eb88'
-// );
-// getIdeas();
-// console.log(sortedByDonationSumIdeas());
-//
-//DonationData Tests
-//
-// donationData(
-//     'Darth',
-//     10,
-//     '6fba75be-1f13-449e-b8ee-0d9287d70208',
-//     '4e94a388-268d-46c9-b888-b2279c469ecd'
-// // );
-// donationData(
-//     null,
-//     10,
-//     '6fba75be-1f13-449e-b8ee-0d9287d70208',
-//     '4e94a388-268d-46c9-b888-b2279c469ecd'
-// );
-// donationData('Vladimir', 10, null, '4e94a388-268d-46c9-b888-b2279c469ecd');
-// donationData(null, 10, null, '4e94a388-268d-46c9-b888-b2279c469ecd');
-//
-//RegisterUser Test
-// registerUser('MasterYoda', '123', 'Master', 'Yoda');
-// registerUser('AnonymousDroid', '123', null, 'Droid');
-// registerUser('MrSpaceNalls', '123', 'Mr', null);
-// registerUser('Chewbacca', '123', null, null);
+Testing createIdea function
+createIdeas(
+    'Another simply awesome header',
+    'Fantastic description of simplu magnificent idea!',
+    250,
+    'a9670cd0-85fa-4847-966b-ab10d58e7a67'
+);
+createIdeas(
+    null,
+    'Fantastic description of simplu magnificent idea!',
+    250,
+    'a9670cd0-85fa-4847-966b-ab10d58e7a67'
+);
+createIdeas(
+    'Another simply awesome header',
+    null,
+    250,
+    'a9670cd0-85fa-4847-966b-ab10d58e7a67'
+);
+createIdeas(
+    'Another simply awesome header',
+    'Fantastic description of simplu magnificent idea!',
+    null,
+    'a9670cd0-85fa-4847-966b-ab10d58e7a67'
+);
+createIdeas(
+    'Another simply awesome header',
+    'Fantastic description of simplu magnificent idea!',
+    250,
+    null
+);
+ */
