08d0edcb5a7ca1408bd34097ab906e6b83b203f5
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-04-25 18:38:14 +0300

    FR-44 Create login func. for reg. users. Tested

--- businessRules/users.js
+++ businessRules/users.js
@@ -1,4 +1,7 @@
+const { v4 } = require('uuid');
+const md5 = require('md5');
 const { createUser } = require('../models/users');
+const { getUsers, setUsers } = require('../utils/storage');
 
 function registerUser(userName, password, firstName, lastName) {
     if (firstName === null && lastName === null) {
@@ -20,6 +23,33 @@ function registerUser(userName, password, firstName, lastName) {
     }
 }
 
+function login(req, res) {
+    let users = getUsers();
+
+    const userName = req.body.userName;
+    const password = md5(req.body.password);
+
+    const user = users.find(
+        (u) => u.userName === userName && u.password === password
+    );
+
+    if (user) {
+        const sessionId = md5(v4()); //Should be REAL cryptography.
+        user.session = sessionId;
+        setUsers(users);
+        res.cookie('userLoginSession', sessionId, {
+            sameSite: 'None',
+            secure: true,
+            httpOnly: true,
+            maxAge: 900000,
+        });
+        res.status(200).json({ status: 'OK', username: user.userName });
+    } else {
+        res.status(400).json({ status: 'No such user.' });
+    }
+}
+
 module.exports = {
     registerUser,
+    login,
 };
