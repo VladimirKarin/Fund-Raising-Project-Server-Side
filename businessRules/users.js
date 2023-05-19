d19b39c0ec8ac384dfe602032b4ffd28f0420617
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-05-19 15:03:13 +0300

    FR-71 Create method that upd. user info. Tested

--- businessRules/users.js
+++ businessRules/users.js
@@ -2,7 +2,6 @@ const { v4 } = require('uuid');
 const md5 = require('md5');
 const { createUser } = require('../models/users');
 const { getUsers, setUsers } = require('../utils/storage');
-const { throws } = require('assert');
 
 function registerUser(userName, password, firstName, lastName) {
     if (!userName) {
@@ -29,8 +28,7 @@ function login(req, res) {
     );
 
     if (!user) {
-        res.status(400).json({ status: 'No such user.' });
-        return;
+        throw new Error('No such user.');
     }
 
     const sessionId = md5(v4()); //Should be REAL cryptography.
@@ -42,15 +40,42 @@ function login(req, res) {
         httpOnly: true,
         maxAge: 900000,
     });
-    res.status(200).json({ status: 'OK', username: user.userName });
 }
 
 function usersList() {
     return getUsers();
 }
 
+function updateUser(userId, key, value) {
+    let users = getUsers();
+
+    const user = users.find((user) => userId === user.id);
+
+    if (!user) {
+        throw new Error('No such user.');
+    }
+
+    const updatedUser = {
+        ...user,
+        [key]: value,
+    };
+
+    function reduceFunction(previousValue, user) {
+        if (user.id === userId) {
+            return [...previousValue, updatedUser];
+        }
+        return [...previousValue, user];
+    }
+
+    const initialReduceValue = [];
+    const updatedUsers = users.reduce(reduceFunction, initialReduceValue);
+
+    setUsers(updatedUsers);
+}
+
 module.exports = {
     registerUser,
     login,
     usersList,
+    updateUser,
 };
