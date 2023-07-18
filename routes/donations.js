271cb6289f8da53852c3db5ba008d7be5dfcfda6
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-07-19 00:19:50 +0300

    Users route update with the help of phind

--- routes/donations.js
+++ routes/donations.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
     try {
         res.status(200).json(getDonationsForIdea(req.query.ideaId));
     } catch (error) {
-        res.status(404).send(error.message);
+        res.status(500).send(error.message);
     }
 });
 
@@ -20,7 +20,7 @@ router.get('/total', (req, res) => {
     try {
         res.status(200).json(getTotalSumDonatedForIdea(req.query.ideaId));
     } catch (error) {
-        res.status(404).send(error.message);
+        res.status(500).send(error.message);
     }
 });
 
--- routes/users.js
+++ routes/users.js
@@ -9,27 +9,36 @@ const {
 } = require('../businessRules/users');
 const { deleteUser } = require('../models/users');
 
-router.get('/', (req, res) => {
-    if (req.body.userLoginSession) {
-        if (checkIfAdmin(req.body.userLoginSession)) {
-            res.status(200).json(getUsers());
-        } else {
-            res.status(403).send(
-                'Access Forbidden. Only Admin has the right to see the users list.'
-            );
-        }
+const isAdmin = (req, res, next) => {
+    const userLoginSession = req.query.userLoginSession;
+
+    if (!userLoginSession) {
+        res.status(403).send(
+            'Access Forbidden. User Login Session not provided.'
+        );
+        return;
+    }
+
+    if (checkIfAdmin(userLoginSession)) {
+        next();
     } else {
         res.status(403).send(
             'Access Forbidden. Only Admin has the right to see the users list.'
         );
     }
+};
+
+router.get('/', isAdmin, (_, res) => {
+    res.status(200).json(getUsers());
 });
 
 router.get('/donations', (req, res) => {
     try {
-        res.status(200).json(getUsersDonations(req.query.userId));
+        const userId = req.query.userId;
+        const donations = getUsersDonations(userId);
+        res.json(donations);
     } catch (error) {
-        res.status(400).send(error.message);
+        res.status(500).send(error.message);
     }
 });
 
@@ -49,20 +58,21 @@ router.post('/', (req, res) => {
 
 router.put('/', (req, res) => {
     try {
-        updateUser(req.body.userId, req.body.key, req.body.value);
+        const { userId, key, value } = req.body;
+        updateUser(userId, key, value);
+        res.status(200).send('User successfully updated.');
     } catch (error) {
         res.status(400).send(error.message);
     }
-    res.status(200).send('User successfully update.');
 });
 
 router.delete('/:userId', (req, res) => {
     try {
         deleteUser(req.params.userId);
+        res.status(200).send('User successfully deleted.');
     } catch (error) {
         res.status(400).send(error.message);
     }
-    res.status(200).send('User successfully deleted.');
 });
 
 module.exports = router;
